2414aea813e481ecc30fa35a2a3e113b2f927606
lenabenninger <lena.benninger@studentfr.ch>
2024-09-26 14:46:20 +0200

    remplir quiz

--- src/js/main.js
+++ src/js/main.js
@@ -5,45 +5,69 @@
 
 // 1. Expliquez ce qu'est-ce qu'une variable.
 // REPONSE:
+// Un conteneur dans lequel on stocke des informations.
+// Au lieu de réécrire à chaque fois le contenu de la variable, on peut simplement y mettre le nom de la variable.
 
 // -----------------------
 // 2. Déclarez une variable s'appelant "firstName" et dont la valeur est votre prénom.
 
+const firstName = "Léna";
+
 // -----------------------
 // 3. Déclarez une variable s'appelant "age" et dont la valeur est votre âge. Attention, sa valeur pourrait être modifiée par la suite.
 
+let age = "19";
+
 // -----------------------
 // 4. Dans le code HTML (lignes 17 - 23), citez les divers éléments composant l'élément HTML <input>:
-// - Balise HTML:
-// - Classe(s):
-// - Attribut(s):
+// - Balise HTML: input
+// - Classe(s): card-input
+// - Attribut(s): type, name, placeholder, id, class
 
 // -----------------------
 // 5. Expliquez la différence entre une class et un ID.
 // REPONSE:
-
+// class = plusieurs fois sur une page
+// id = unique
 // -----------------------
 // 6. A quoi sert la méthode document.querySelector("..."); ?
 // REPONSE:
+// Pour sélectionner un élément qui se trouve dans le html depuis Javascript.
 
 // -----------------------
 // 7. A l'aide de la méthode document.querySelector("..."), sélectionnez l'élément <p> (ligne 14 du code HTML):
 // 7.1 Créez une variable appelée "cardContent".
 // 7.2 Assignez lui l'élément <p> (ligne 14 du code HTML) que vous avez sélectionné en 7.1.
 
+const cardContent = document.querySelector(".card-content");
+
 // -----------------------
 // 8. Utilisez la variable "cardContent" créée à l'exercice 7:
 // 8.1 Utilisez la propriété   -> .classList <-   pour ajouter   -> .add("...") <-   la class "highlight" à l'élément HTML.
 
+cardContent.classList.add("highlight");
+
 // -----------------------
 // 9. Récupérez la valeur de l'attribut "placeholder"   -> .getAttribute("...") <-   de l'élément HTML <input> (ligne 17 du code HTML).
+
+const input = document.querySelector("#password-input");
+
+input.getAttribute("placeholder");
 
 // -----------------------
 // 10. Expliquez ce qu'est un écouteur d'événement.
 // REPONSE:
+//Une fonction qui attend que quelque chose se passe et qui réagit en fonction de ce qu'on lui dit de faire.
 
 // -----------------------
 // 11. Sur l'élément HTML button (ligne 26 du code HTML), écoutez l'événement "click":
 // 11.1 : Sélectionnez l'élément HTML <button> (ligne 26 du code HTML).
 // 11.2 : Utilisez la méthode   -> .addEventListener("...", function() {...}) <-   sur l'élément HTML sélectionné en 11.1 pour écouter l'événement "click".
 // 11.3 : Lorsqu'un.e utilisateurice clique sur le bouton, ajoutez/supprimez (toggle) la class "hide" de l'élément HTML <p> (ligne 29-31 du code HTML).
+
+const btn = document.querySelector("#card-btn");
+const toggleText = document.querySelector("#toggle-text");
+
+btn.addEventListener("click", function () {
+  toggleText.classList.toggle("hide");
+});
